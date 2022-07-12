45c3ae2bd40db35c0b47acdda643c5287d6c5836
korchemkinsw <korchemkinsw@yandex.ru>
2022-07-12 14:20:57 +0300

    redirect

--- Project_A/views.py
+++ Project_A/views.py
@@ -1,18 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
 from . import settings
 
+User = get_user_model()
 
 @login_required
 def index(request):
+    if request.user.role == User.SECRETARY or request.user.role == User.ENGINEER:
+        return redirect('/orders/')
     context={
         'my_company': settings.MYCOMPANY,
         'url': 'index'
         }
     return render(request, "index.html", context)
+    
 
 def guide(request):
     context={
